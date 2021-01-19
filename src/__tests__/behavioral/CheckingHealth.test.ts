ea7ccc4cb2b93777028b256328b93d1644e439bc
Taylor Romero <me@taylorrome.ro>
2021-01-19 09:11:18 -0700

    patch: lint

--- src/__tests__/behavioral/CheckingHealth.test.ts
+++ src/__tests__/behavioral/CheckingHealth.test.ts
@@ -1,4 +1,4 @@
-import AbstractSpruceTest, { test, assert } from '@sprucelabs/test'
+import { test, assert } from '@sprucelabs/test'
 import { errorAssertUtil } from '@sprucelabs/test-utils'
 import plugin from '../../plugins/conversation.plugin'
 import AbstractConversationTest from '../../tests/AbstractConversationTest'
